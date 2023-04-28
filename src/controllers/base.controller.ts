17dcf554caa13e161b8c44fa24c3b4bd90315e5b
phamquyet9688 <117346649+phamquyet9688@users.noreply.github.com>
2023-04-28 21:51:07 +0700

    fix be

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -17,7 +17,7 @@ class BaseController {
     }
 
     static generateAccessToken(payload) {
-        return jwt.sign(payload, `${process.env.JWT_SECRET_KEY}`, {expiresIn: "1y"}
+        return jwt.sign(payload, `${process.env.JWT_SECRET_KEY}`, {expiresIn: "60m"}
         );
     }
 
