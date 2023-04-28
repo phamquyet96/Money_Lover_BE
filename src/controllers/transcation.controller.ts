02e4ad95c85ebd0cdf8cc1832aaffa207c2d864f
Phạm Việt Trung <phvitrung@gmail.com>
2023-04-28 15:36:47 +0700

    calculate-balance

--- src/controllers/transcation.controller.ts
+++ src/controllers/transcation.controller.ts
@@ -1,97 +1,111 @@
 import BaseController from "./base.controller";
-import dataSource from "../database/data-source"
-import TransactionModel, { Transaction } from "../models/transaction.model";
-import Wallet from "../models/wallet.model";
-import SubCate from "../models/trans.subcate.model";
 import TransactionServices from "../services/transaction.services";
 import WalletServices from "../services/wallet.services";
 
-import { Request, Response } from "express";
 
+
+import {Request, Response} from "express";
+import WalletModel from "../models/wallet.model";
+import dataSource from "../database/data-source";
+import Wallet from "../models/wallet.model";
+let walletRepo = dataSource.getRepository(Wallet);
 const [INCOME, EXPENSE] = ["Income", "Expense"];
 
 class TransactionController extends BaseController {
 
-  static getTransactions(req: any, res: Response) {
-    let walletId = req.query.walletId;
-    let startDate = req.query.startDate;
-    let endDate = req.query.endDate;
+    static getTransactions(req: any, res: Response) {
+        let walletId = req.query.walletId;
+        let startDate = req.query.startDate;
+        let endDate = req.query.endDate;
 
-    TransactionServices.getTransactionOfUserByTime(walletId, startDate, endDate)
-      .then(result => {
-        res.json(result);
-      })
-  }
-
-  static getTransactionsOfOneWallet(req: Request, res: Response) {
-    let walletId = req.params.walletId;
-    TransactionServices.getTransactionsOfWallet(walletId, req.query)
-      .then(transactions => {
-        res.json(transactions);
-      })
-  }
-
-  static getTransactionsByTypeNameOfAllWallets(req: any, res: Response) {
-    let userId = req.user.id;
-    TransactionServices.getTransactionsByTypeName(userId,req.query)
-      .then(transactions => {
-        res.json(transactions)
-      })
-  }
-
-  static getTransactionsByTypeNameOfOneWallet(req: Request, res: Response) {
-    let walletId = req.params.walletId;
-    TransactionServices.getTransactionsByTypeNameOfWallet(walletId, req.query)
-      .then(transactions => {
-        res.json(transactions)
-      })
-  }
-
-  static async addTransaction(req: Request, res: Response) {
-    try {
-      let { walletId, subcategoryId, money, date, image, note } = req.body
-      await TransactionServices.addTransaction(walletId, subcategoryId, money, date, image, note);
-      // await WalletServices.updateBalance(walletId);
-      res.status(200).json({ message: "Added transaction successfully" });
+        TransactionServices.getTransactionOfUserByTime(walletId, startDate, endDate)
+            .then(result => {
+                res.json(result);
+            })
     }
-    catch (err) {
-      console.log(err);
-      res.status(500).json({ message: err.message });
-    }
-  }
 
-  static async updateTransaction(req: Request, res: Response) {
-    try {
-      let transactionId = Number(req.params.transactionId);
-      let transaction = await TransactionServices.getTransactionById(transactionId);
-      let previousWalletId = transaction.wallet.id;
-      let currentWalletId = req.body.walletId;
-      await TransactionServices.updateTransaction(transactionId, req.body);
-      await WalletServices.updateBalance(previousWalletId);
-      if (previousWalletId !== currentWalletId) {
-        await WalletServices.updateBalance(currentWalletId);
-      }
-      res.status(200).json({message: "Updated transaction successfully"});
+    static getTransactionsOfOneWallet(req: Request, res: Response) {
+        let walletId = req.params.walletId;
+        TransactionServices.getTransactionsOfWallet(walletId, req.query)
+            .then(transactions => {
+                res.json(transactions);
+            })
     }
-    catch (err) {
-      res.status(500).json(err.message);
-    }
-  }
 
-  static async deleteTransaction(req: Request, res: Response) {
-    try {
-      console.log(req.params.transactionId)
-      let transactionId = Number(req.params.transactionId);
-      let transaction = await TransactionServices.getTransactionById(transactionId);
-      let walletId = transaction.wallet.id;
-      await TransactionServices.deleteTransaction(transaction);
-      await WalletServices.updateBalance(walletId);
-      res.status(200).json({message: "Deleted transaction successfully"})
+    static getTransactionsByTypeNameOfAllWallets(req: any, res: Response) {
+        let userId = req.user.id;
+        TransactionServices.getTransactionsByTypeName(userId, req.query)
+            .then(transactions => {
+                res.json(transactions)
+            })
     }
-    catch (err) {
-      res.status(500).json({message: err.message})
+
+    static getTransactionsByTypeNameOfOneWallet(req: Request, res: Response) {
+        let walletId = req.params.walletId;
+        TransactionServices.getTransactionsByTypeNameOfWallet(walletId, req.query)
+            .then(transactions => {
+                res.json(transactions)
+            })
     }
-  }
+
+    static async addTransaction(req: Request, res: Response) {
+        try {
+            let {walletId, subcategoryId, money, date, image, note} = req.body
+            await TransactionServices.addTransaction(walletId, subcategoryId, money, date, image, note);
+            await this.calculateBalance(walletId, money, subcategoryId)
+            // await WalletServices.updateBalance(walletId);
+            res.status(200).json({message: "Added transaction successfully"});
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({message: err.message});
+        }
+    }
+
+    static async calculateBalance(walletId: number, money: number, subcategoryId: number) {
+        try {
+            console.log(walletId, money, subcategoryId)
+            // const wallet = await walletRepo.findOneByOrFail({id: walletId});
+            // if (subId == 1) {
+            //     wallet.balance = wallet.balance + money;
+            // } else {
+            //     wallet.balance = wallet.balance - money;
+            // }
+            // return walletRepo.save(wallet);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    static async updateTransaction(req: Request, res: Response) {
+        try {
+            let transactionId = Number(req.params.transactionId);
+            let transaction = await TransactionServices.getTransactionById(transactionId);
+            let previousWalletId = transaction.wallet.id;
+            let currentWalletId = req.body.walletId;
+            await TransactionServices.updateTransaction(transactionId, req.body);
+            await WalletServices.updateBalance(previousWalletId);
+            if (previousWalletId !== currentWalletId) {
+                await WalletServices.updateBalance(currentWalletId);
+            }
+            res.status(200).json({message: "Updated transaction successfully"});
+        } catch (err) {
+            res.status(500).json(err.message);
+        }
+    }
+
+    static async deleteTransaction(req: Request, res: Response) {
+        try {
+            console.log(req.params.transactionId)
+            let transactionId = Number(req.params.transactionId);
+            let transaction = await TransactionServices.getTransactionById(transactionId);
+            let walletId = transaction.wallet.id;
+            await TransactionServices.deleteTransaction(transaction);
+            await WalletServices.updateBalance(walletId);
+            res.status(200).json({message: "Deleted transaction successfully"})
+        } catch (err) {
+            res.status(500).json({message: err.message})
+        }
+    }
 
 }
 
--- src/services/transcate.services.ts
+++ src/services/transcate.services.ts
@@ -1,26 +1,16 @@
 import BaseServices from "./base.services";
 import dataSource from "../database/data-source";
 import TransCate from "../models/trans.cate.model";
-import User from "../models/user.model";
 
 let transCateRepo = dataSource.getRepository(TransCate);
 
 class TransCateServices extends BaseServices {
-  static async getAllCates(user: User): Promise<TransCate[]> {
-    let result = await transCateRepo.find({
+  static async getAllCates(): Promise<TransCate[]> {
+    return await transCateRepo.find({
       relations: {
         subCategories: true,
-        transType: true,
-      },
-      where: {
-        subCategories: {
-          user: {
-            id: user.id,
-          }
-        }
-      },
+      }
     });
-    return result;
   }
 }
 
